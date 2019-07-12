25f02e1efb3efc4687e7dd5112476573c490f4f3
node0 <node0@coralnode.com>
2019-07-12 13:34:55 +0200

    first commit

---
+++ lib/microsd_lib/src/microsd_lib.h
@@ -0,0 +1,21 @@
+
+#ifndef microsd_lib_h
+
+#define microsd_lib_h
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
+void createDir(fs::FS &fs, const char * path);
+void removeDir(fs::FS &fs, const char * path);
+void readFile(fs::FS &fs, const char * path);
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char * path, const char * message);
+void renameFile(fs::FS &fs, const char * path1, const char * path2);
+void deleteFile(fs::FS &fs, const char * path);
+void testFileIO(fs::FS &fs, const char * path);
+void setupMicrosd();
+
+#endif
